8c24b0d84e35885082cf90f25a5cb3b1f90ffb46
antonVosc <anton.vosc@gmail.com>
2025-01-28 22:03:30 +0000

    Created navigation menu for mobile version

---
+++ src/components/Navbar/MobileNav/MobileNav.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./MobileNav.css";
+
+const MobileNav = ({ isOpen, toggleMenu }) => { 
+  return (
+    <>
+      <div className={`mobile-menu ${ isOpen ? "active" : "" }`} onClick={ toggleMenu }>
+        <div className="logo-container" style={{ backgroundColor: isOpen ? "#342864" : "transparent", padding: isOpen ? "1rem" : "0", }}>
+          <img className="logo" src="./assets/images/logo.png" alt="Logo" />
+        </div>
+
+        <div className="purple-space" style={{ backgroundColor: isOpen ? "#342864" : "transparent", position: "absolute", height: "50px", width: "240px", }}></div>
+
+        <div className="mobile-menu-container">
+          <ul>
+            <li>
+              <a className="menu-item">Home</a>
+            </li>
+
+            <li>
+              <a className="menu-item">Skills</a>
+            </li>
+
+            <li>
+              <a className="menu-item">Education</a>
+            </li>
+
+            <li>
+              <a className="menu-item">Work Experience</a>
+            </li>
+
+            <li>
+              <a className="menu-item">My Projects</a>
+            </li>
+
+            <button className="contact-btn" onClick={() => { toggleMenu(); }}>
+              Contact Me
+            </button>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MobileNav;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import './Navbar.css';
+import React, { useState } from "react";
+import "./Navbar.css";
+import MobileNav from "./MobileNav/MobileNav";
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
   return (
     <>
+      <MobileNav isOpen={ openMenu } toggleMenu={ toggleMenu } />
+
       <nav className="nav-wrapper">
         <div className="nav-content">
-          <img className="logo" src="./assets/images/logo.png" alt="" />
+          <img className={`logo ${openMenu ? 'logo-active' : ''}`} src="./assets/images/logo.png" alt="" />
 
           <ul>
             <li>
@@ -34,8 +42,10 @@ const Navbar = () => {
             </button>
           </ul>
 
-          <button class="menu-btn" onClick={() => {}}>
-            <span class={"material-symbols-outlined"} style={{ fontSize: "1.8rem" }}>menu</span>
+          <button class="menu-btn" onClick={ toggleMenu }>
+            <span class={"material-symbols-outlined"} style={{ fontSize: "1.8rem" }}>
+              { openMenu ? "close" : "menu" }
+            </span>
           </button>
         </div>
       </nav>
