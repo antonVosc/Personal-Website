830d387d74d1b07400cecd3a2e7c1a163fc5c8ff
antonVosc <anton.vosc@gmail.com>
2025-02-01 23:48:01 +0000

    Added skills with bars and percentages

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Skills.css";
 import { SKILLS } from "../../utils/data";
 import SkillCard from "./SkillCard/SkillCard";
+import SkillsInfoCard from "./SkillsInfoCard/SkillsInfoCard";
 
 const Skills = () => {
+  const [selectedSkill, setSelectedSkill] = useState(SKILLS[0]);
+  const handleSelectSkill = (data) => {
+    setSelectedSkill(data);
+  };
+
   return (
     <section className="skills-container">
         <h5>Technical Profficiency</h5>
@@ -11,11 +17,13 @@ const Skills = () => {
         <div className="skills-content">
             <div className="skills">
               { SKILLS.map((item) => (
-                <SkillCard key={ item.title } iconUrl={ item.icon } title={ item.title } />
+                <SkillCard key={ item.title } iconUrl={ item.icon } title={ item.title } isActive={ selectedSkill.title === item.title } onClick={() => { handleSelectSkill(item); }} />
               ))}
             </div>
 
-            <div className="skills-info"></div>
+            <div className="skills-info">
+              <SkillsInfoCard heading={ selectedSkill.title } skills={ selectedSkill.skills } />
+            </div>
         </div>
     </section>
   );
