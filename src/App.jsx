cf322bb75acf39a81b061bd8f4ae2ccdbceb51c5
antonVosc <anton.vosc@gmail.com>
2025-02-23 15:55:50 +0000

    Trying to add Google tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ReactGA from "react-ga";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
