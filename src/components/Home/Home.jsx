2ca060d22cc1b505b24662c6135dab33952c60fb
Anton Voshchinskiy <anton.vosc@gmail.com>
2026-02-18 21:39:28 +0100

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
   const STATS = [
           { top: "Years of", value: 2, suffix: "+", bottom: "Experience" },
           { top: "Projects", value: 20 , suffix: "+", bottom: "Completed" },
-          { top: "People Visited Website", value: 19, bottom: "in "+new Date().toLocaleString("en-US", { month: "long" }) },
-          { top: "People Visited Website", value: 48, bottom: "in "+new Date().getFullYear() },
+          { top: "People Visited Website", value: 26, bottom: "in "+new Date().toLocaleString("en-US", { month: "long" }) },
+          { top: "People Visited Website", value: 55, bottom: "in "+new Date().getFullYear() },
         ];
   
   return (
