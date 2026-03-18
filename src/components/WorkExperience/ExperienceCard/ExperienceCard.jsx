cf0b99cf77648d1f27a2afccfb5114480ec1bc16
Anton Voshchinskiy <anton.vosc@gmail.com>
2026-03-18 15:58:42 +0000

    Refactor ExperienceCard component structure

--- src/components/WorkExperience/ExperienceCard/ExperienceCard.jsx
+++ src/components/WorkExperience/ExperienceCard/ExperienceCard.jsx
@@ -2,26 +2,25 @@ import React from "react";
 import "./ExperienceCard.css";
 
 const ExperienceCard = ({ details }) => {
-  return (
+  const content = (
     <div className="work-experience-card">
-        <h6>{ details.title }</h6>
-        <div className="work-duration">{ details.date }</div>
-        <div className="company">
-          {details.link ? (
-            <a href={ details.link } target="_blank" rel="noreferrer">
-              { details.company }
-            </a>
-          ) : (
-            details.company
-          )}
-        </div>
-
-        <ul>
-            { details.responsibilities.map((item) => (
-                <li key={ item }>{ item }</li>
-            )) }
-        </ul>
+      <h6>{details.title}</h6>
+      <div className="work-duration">{details.date}</div>
+      <div className="company">{details.company}</div>
+      <ul>
+        {details.responsibilities.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
     </div>
+  );
+
+  return details.link ? (
+    <a href={details.link} target="_blank" rel="noreferrer" style={{ textDecoration: "none", color: "inherit" }}>
+      {content}
+    </a>
+  ) : (
+    content
   );
 };
 
